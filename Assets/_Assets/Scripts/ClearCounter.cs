87f3577a92eddb497485925e37c302047ba5633f
DreammyGuy <91828332+DreammyGuy@users.noreply.github.com>
2025-06-16 17:17:44 +0700

    Pickup, Drop Object
    Pickup, Drop Object, Add more Container Counter

--- Assets/_Assets/Scripts/ClearCounter.cs
+++ Assets/_Assets/Scripts/ClearCounter.cs
@@ -7,6 +7,31 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-
+        if (!HasKitchenObject())
+        {
+            // There is no kitchen object on ClearCounter
+            if (player.HasKitchenObject())
+            {
+                // Player has a kitchen object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player has no kitchen object
+            }
+        }
+        else
+        {
+            // There is a kitchen object on ClearCounter
+            if (player.HasKitchenObject())
+            {
+                // Player has a kitchen object
+            }
+            else
+            {
+                // Player has no kitchen object
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
--- Assets/_Assets/Scripts/ContainerCounter.cs
+++ Assets/_Assets/Scripts/ContainerCounter.cs
@@ -9,8 +9,9 @@ public class ContainerCounter : BaseCounter
     public event EventHandler OnPlayerGrabbedObject;
     public override void Interact(Player player)
     {
-        if (!HasKitchenObject())
+        if (!player.HasKitchenObject())
         {
+            // Player has no kitchen object
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
             OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
